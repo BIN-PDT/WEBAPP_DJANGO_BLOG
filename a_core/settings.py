2b3dc932e84103a683fd0da1a5f00bca1405a83c
BIN BIN <thangpd300502@gmail.com>
2025-03-27 11:32:39 +0700

    ADD SOCIAL ACCOUNT

--- a_core/settings.py
+++ a_core/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
+
+
+env = Env()
+env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,6 +53,10 @@ INSTALLED_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+    "allauth.socialaccount.providers.github",
+    "allauth.socialaccount.providers.twitter",
+    "allauth.socialaccount.providers.facebook",
     "a_home",
     "a_user",
     "a_blog",
@@ -98,6 +108,51 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "a_core.wsgi.application"
+
+
+# SOCIAL ACCOUNT.
+SOCIALACCOUNT_PROVIDERS = {
+    "google": {
+        "APP": {
+            "client_id": env("OAUTH_GOOGLE_CLIENT_ID"),
+            "secret": env("OAUTH_GOOGLE_CLIENT_SECRET"),
+        },
+        "SCOPE": ["profile", "email"],
+        "AUTH_PARAMS": {"access_type": "online", "prompt": "consent"},
+    },
+    "github": {
+        "APP": {
+            "client_id": env("OAUTH_GITHUB_CLIENT_ID"),
+            "secret": env("OAUTH_GITHUB_CLIENT_SECRET"),
+        }
+    },
+    "twitter": {
+        "APP": {
+            "client_id": env("OAUTH_TWITTER_CLIENT_ID"),
+            "secret": env("OAUTH_TWITTER_CLIENT_SECRET"),
+        }
+    },
+    "facebook": {
+        "APP": {
+            "client_id": env("OAUTH_FACEBOOK_CLIENT_ID"),
+            "secret": env("OAUTH_FACEBOOK_CLIENT_SECRET"),
+        }
+    },
+}
+
+SOCIALACCOUNT_LOGIN_ON_GET = True
+
+SOCIALACCOUNT_AUTO_SIGNUP = True
+
+SOCIALACCOUNT_EMAIL_AUTHENTICATION = False
+
+SOCIALACCOUNT_EMAIL_AUTHENTICATION_AUTO_CONNECT = False
+
+SOCIALACCOUNT_EMAIL_VERIFICATION = "none"
+
+ACCOUNT_ADAPTER = "a_user.adapters.CustomAccountAdapter"
+
+SOCIALACCOUNT_ADAPTER = "a_user.adapters.CustomSocialAccountAdapter"
 
 
 # Database
